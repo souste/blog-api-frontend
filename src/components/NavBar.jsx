f95ca7248ec74d104a9742fb59ea003f8669d5d3
Stephen <stephensouness@outlook.com>
2025-03-19 16:15:44 +0000

    created a logout button and wrapped it in a current user ternary. Signup and logout only visible if no user is logged in

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,18 +8,25 @@ const NavBar = () => {
     <nav className="navbar navbar-expand-lg navbar-light bg-light shadow-sm p-3">
       <div className="container">
         <h1 className="navbar-brand text-primary fs-2">
-          Welcome {currentUser ? currentUser.username : "Outside User"} to the Blog
+          Welcome to the Blog {currentUser ? currentUser.username : "Outside User"}
         </h1>
         <div className="d-flex ms-auto">
           <Link to="/create-post" className="btn btn-success btn-lg px-4 me-3">
             ➕ Create Post
           </Link>
-          <Link to="/sign-up" className="btn btn-outline-primary btn-lg px-4 me-3">
-            📝 Sign Up
-          </Link>
-          <Link to="/login" className="btn btn-outline-primary btn-lg px-4">
-            📝 Login
-          </Link>
+
+          {currentUser ? (
+            <button>🚪 Log Out</button>
+          ) : (
+            <div>
+              <Link to="/sign-up" className="btn btn-outline-primary btn-lg px-4 me-3">
+                📝 Sign Up
+              </Link>
+              <Link to="/login" className="btn btn-outline-primary btn-lg px-4">
+                📝 Login
+              </Link>
+            </div>
+          )}
         </div>
       </div>
     </nav>
